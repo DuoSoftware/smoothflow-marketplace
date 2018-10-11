d797acca801db983bbe3300d4d2a086b120bb170
KasunW-duo <kasun.w@duosoftware.com>
2018-10-11 13:06:51 +0530

    Restructured for the build

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -212,6 +212,10 @@ module.exports = {
             ),
             // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
           },
+          {
+            test: /\.scss$/,
+            loader: ExtractTextPlugin.extract('css-loader!sass-loader', 'css?importLoaders=1&-autoprefixer!postcss!sass')
+          },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
           // This loader doesn't use a "test" so it will catch all modules
@@ -222,7 +226,7 @@ module.exports = {
             // it's runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/, /\.scss$/],
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
             },
